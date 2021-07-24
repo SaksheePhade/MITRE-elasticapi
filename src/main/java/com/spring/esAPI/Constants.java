c7a7ba78ea0cbabf14d5b382ede6c8062bcc7d00
Ajay Kumar <ajay.kumar.tbv@gmail.com>
2021-07-24 15:05:41 +0000

    elastic search url

--- src/main/java/com/spring/esAPI/Constants.java
+++ src/main/java/com/spring/esAPI/Constants.java
@@ -2,6 +2,6 @@ package com.spring.esAPI;
 
 public class Constants {
 
-	public static final String ELASTIC_URL = "localhost:9200";
+	public static final String ELASTIC_URL = "dev-els-es-http.default.svc.cluster.local:9200";
     public static final String MITRE_INDEX = "mitretest";
 }
