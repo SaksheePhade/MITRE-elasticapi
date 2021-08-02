845f6e3b7c1284622e833340660f06df747c56ad
Ellika Mishra <ellikamishra009@gmail.com>
2021-08-02 06:57:36 +0000

    Constants.java edited online with Bitbucket

--- src/main/java/com/spring/esAPI/Constants.java
+++ src/main/java/com/spring/esAPI/Constants.java
@@ -2,6 +2,6 @@ package com.spring.esAPI;
 
 public class Constants {
 
-	public static final String ELASTIC_URL = "localhost:9200";
-    public static final String MITRE_INDEX = "mitre123";
+	public static final String ELASTIC_URL = "quickstart-es-http.default.svc.cluster.local:9200";
+    public static final String MITRE_INDEX = "mitrenew";
 }
