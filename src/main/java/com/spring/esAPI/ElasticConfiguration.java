5647774420a4c12b352eff52d954700ec2e360fd
Yash Jagdale <yjagdale@securonix.com>
2021-08-03 21:17:52 +0530

    elastic with ssl

--- src/main/java/com/spring/esAPI/ElasticConfiguration.java
+++ src/main/java/com/spring/esAPI/ElasticConfiguration.java
@@ -21,10 +21,11 @@ public class ElasticConfiguration {
     public RestHighLevelClient elasticsearchClient() {
         String userName = Objects.isNull(System.getenv("ELS_USER_NAME")) ? Constants.ELS_USERNAME : System.getenv("ELS_USER_NAME");
         String password = Objects.isNull(System.getenv("ELS_PASSWORD")) ? Constants.ELS_PASSWORD : System.getenv("ELS_USER_NAME");
-        String elsURL = Objects.isNull(System.getenv("ELS_URL")) ? Constants.ELS_URL : System.getenv("ELS_USER_NAME");
+        String elsURL = Objects.isNull(System.getenv("ELS_URL")) ? Constants.ELS_URL : System.getenv("ELS_URL");
         logger.info("Connecting to els " + elsURL + " with username: " + userName);
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo(elsURL)
+                .usingSsl()
                 .withBasicAuth(userName, password)
                 .build();
         logger.info("Connected to els successfully");
