fefa03df7abbee97da3f4f55b2bb8d02ec5b18fd
Yash Jagdale <yjagdale@securonix.com>
2021-08-03 20:29:58 +0530

    added username and password support in els client

--- src/main/java/com/spring/esAPI/Constants.java
+++ src/main/java/com/spring/esAPI/Constants.java
@@ -2,6 +2,8 @@ package com.spring.esAPI;
 
 public class Constants {
 
-	public static final String ELASTIC_URL = "quickstart-es-http.default.svc.cluster.local:9200";
     public static final String MITRE_INDEX = "mitrenew";
+    public static final String ELS_USERNAME = "elastic";
+    public static final String ELS_PASSWORD = "password";
+    public static final String ELS_URL = "quickstart-es-http.default.svc.cluster.local:9200";
 }
--- src/main/java/com/spring/esAPI/ElasticConfiguration.java
+++ src/main/java/com/spring/esAPI/ElasticConfiguration.java
@@ -7,18 +7,23 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
+import java.util.Objects;
 
 
 @Configuration
 public class ElasticConfiguration {
-	
-	 	@Bean
-	    public RestHighLevelClient elasticsearchClient() {
 
-	        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-	                .connectedTo(Constants.ELASTIC_URL)
-	                .build();
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        String userName = Objects.isNull(System.getenv("ELS_USER_NAME")) ? Constants.ELS_USERNAME : System.getenv("ELS_USER_NAME");
+        String password = Objects.isNull(System.getenv("ELS_PASSWORD")) ? Constants.ELS_PASSWORD : System.getenv("ELS_USER_NAME");
+        String elsURL = Objects.isNull(System.getenv("ELS_URL")) ? Constants.ELS_URL : System.getenv("ELS_USER_NAME");
 
-	        return RestClients.create(clientConfiguration).rest();
-	    }
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(elsURL)
+                .withBasicAuth(userName, password)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
 }
